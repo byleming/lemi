5a62428804cd84485a550896ce76dd27391512b7
yulong.bai(白玉龙) <yulong.bai@transsion.com>
2019-05-17 09:55:56 +0800

    first commit

---
+++ src/test/java/com/byleming/lemi/LemiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.byleming.lemi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LemiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
